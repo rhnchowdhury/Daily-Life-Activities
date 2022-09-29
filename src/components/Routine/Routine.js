cec05b095b5f53fd6e68423a38b3104f8a7c2284
Raihan <rhnchdri@gmail.com>
2022-09-29 19:52:53 +0600

    total work part

--- src/components/Routine/Routine.js
+++ src/components/Routine/Routine.js
@@ -5,9 +5,14 @@ const Routine = (props) => {
     return (
         <div className='activity-design'>
             <div className='img-design'><img src={picture} alt="" /></div>
-            <h3>{name}</h3>
-            <p>{details}</p>
-            <p>Time: {time}</p>
+            <div className='line-design'>
+                <h3>{name}</h3>
+                <p>{details}</p>
+                <p>Time: <strong>{time}</strong> minutes</p>
+            </div>
+            <button className='btn-design'>
+                <p className='p-design'>Add to list</p>
+            </button>
         </div>
     );
 };
