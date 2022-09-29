f435fa328665dad2b85db3f552c0b250b9d2aa29
Raihan <rhnchdri@gmail.com>
2022-09-29 11:24:13 +0600

    navar part

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.webp'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='navigator'>
+            <img className='image-design' src={logo} alt="" />
+            <h1 className='head-design'>FitProCentre</h1>
+        </nav>
+    );
+};
+
+export default Header;
