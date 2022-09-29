7e1438b0dd333a755e5908f04ce2fbef775b5364
Raihan <rhnchdri@gmail.com>
2022-09-29 12:28:28 +0600

    data create

---
+++ src/components/Activity/Activity.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Activity = () => {
+    return (
+        <div>
+            <div className="div">
+                <h1>daily work</h1>
+            </div>
+            <div className="div">
+                <h1>total work</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Activity;
