aa74f841414778a4d158789a42f82fd70b77e723
Raihan <rhnchdri@gmail.com>
2022-09-29 20:30:33 +0600

    all design part

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -21,51 +21,53 @@ const Activity = () => {
                 }
             </div>
             <div className='cart-design'>
-                <div className='cart-re-design'>
-                    <div className='cart-img'>
-                        <img src={img} alt="" />
+                <div className="cart-margin">
+                    <div className='cart-re-design'>
+                        <div className='cart-img'>
+                            <img src={img} alt="" />
+                        </div>
+                        <div className='decoration'>
+                            <h4 className='name-design'>Raihan Chowdhury</h4>
+                            <p>Web Developer</p>
+                        </div>
                     </div>
-                    <div className='decoration'>
-                        <h4 className='name-design'>Raihan Chowdhury</h4>
-                        <p>Web Developer</p>
+                    <div className='w-h-design'>
+                        <div className='div'>
+                            <p>Weight:</p>
+                            <p><strong className='para-design'>65</strong>kg</p>
+                        </div>
+                        <div className='div'>
+                            <p>Height: </p>
+                            <p><strong className='para-design'>5.7</strong></p>
+                        </div>
+                        <div className='div'>
+                            <p>Age: </p>
+                            <p><strong className='para-design'>25</strong></p>
+                        </div>
                     </div>
+                    <h4>Add A Break</h4>
+                    <div className='second-design'>
+                        <div>
+                            <p className='div-design'>10s</p>
+                        </div>
+                        <div>
+                            <p className='div-design'>20s</p>
+                        </div>
+                        <div>
+                            <p className='div-design div-re-design'>30s</p>
+                        </div>
+                        <div>
+                            <p className='div-design'>40s</p>
+                        </div>
+                        <div>
+                            <p className='div-design'>50s</p>
+                        </div>
+                    </div>
+                    <h4 className='rtn-details'>Routine Details</h4>
+                    <div className='routine'>Routine Time: </div>
+                    <div className='break'>Break Time: </div>
+                    <button className='btn-decorate'>Activity Completed</button>
                 </div>
-                <div className='w-h-design'>
-                    <div className='div'>
-                        <p>Weight:</p>
-                        <p><strong className='para-design'>65</strong>kg</p>
-                    </div>
-                    <div className='div'>
-                        <p>Height: </p>
-                        <p><strong className='para-design'>5.7</strong></p>
-                    </div>
-                    <div className='div'>
-                        <p>Age: </p>
-                        <p><strong className='para-design'>25</strong></p>
-                    </div>
-                </div>
-                <h4>Add A Break</h4>
-                <div className='second-design'>
-                    <div>
-                        <p className='div-design'>10s</p>
-                    </div>
-                    <div>
-                        <p className='div-design'>20s</p>
-                    </div>
-                    <div>
-                        <p className='div-design div-re-design'>30s</p>
-                    </div>
-                    <div>
-                        <p className='div-design'>40s</p>
-                    </div>
-                    <div>
-                        <p className='div-design'>50s</p>
-                    </div>
-                </div>
-                <h4 className='rtn-details'>Routine Details</h4>
-                <div className='routine'>Routine Time: </div>
-                <div className='break'>Break Time: </div>
-                <button className='btn-decorate'>Activity Completed</button>
             </div>
         </div >
     );
